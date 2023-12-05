ca1965dfe9375662edee5f2ae8cade95ef7e850d
Fr3D0o <jofrescari@gmail.com>
2023-12-05 14:04:57 +0100

    netlify config

--- src/globals.js
+++ src/globals.js
@@ -1,3 +1,1 @@
-let port = 3000
-// export const url = `http://localhost:${port}`
-export const url = `http://ec2-35-180-156-215.eu-west-3.compute.amazonaws.com:${port}`
+export const url = `https://localhost:8443`
